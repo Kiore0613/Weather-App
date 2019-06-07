aeba5d15c923b09501773c04b640938ad2e77722
Kiore <ricardochicas95@gmail.com>
2019-06-07 14:52:53 -0600

    API and Volley hhtp

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.weather_app
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +20,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val ciudad = intent.getStringExtra(CiudadesActivity.TAG)
+
+        if(Network.red(this)){
+            solicitudHttpVolley("http://api.openweathermap.org/data/2.5/weather?id=6173331&appid=5f79d7c454fb39f7bc63cf2c1ab67fe8")
+            //5f79d7c454fb39f7bc63cf2c1ab67fe8
+            //ID Vancouver 6173331
+        }
+        else {
+
+        }
 
         val ciudadVc = Ciudad("Vancouver", 16, "Lluvia")
         val ciudadMt = Ciudad("Montreal", 19, "Nublado")
@@ -33,5 +48,23 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    //Metodo para Volley
+
+    private fun solicitudHttpVolley(url:String) {
+
+        val queue = Volley.newRequestQueue(this)
+        val solicitud = StringRequest(Request.Method.GET, url, Response.Listener<String>{
+                response ->
+            try {
+                Log.d("SolicitudVolley", response)
+            }
+            catch (e: Exception){
+
+            }
+        }, Response.ErrorListener{})
+
+        queue.add(solicitud)
     }
 }
